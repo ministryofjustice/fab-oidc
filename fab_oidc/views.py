c5437479cc5ca6555f3f3553d3fc2c44126b7e4f
mr.Shu <mr@shu.io>
2018-10-15 10:44:06 +0200

    update: A couple of updates for Superset
    * Add `SupersetOIDCSecurityManager` that is a subclass of
      `SupersetSecurityManager` which Superset now requires.
    
    * Add custom `SupersetAuthOIDCView` that uses `given_name` and
      `family_name` when adding a user
    
    * Logout the user using the correct endpoint
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- fab_oidc/views.py
+++ fab_oidc/views.py
@@ -51,3 +51,55 @@ class AuthOIDCView(AuthOIDView):
             logout_uri = self.appbuilder.app.config['OIDC_LOGOUT_URI']
 
         return redirect(logout_uri + quote(redirect_url))
+
+class SupersetAuthOIDCView(AuthOIDView):
+
+    @expose('/login/', methods=['GET', 'POST'])
+    def login(self, flag=True):
+
+        sm = self.appbuilder.sm
+        oidc = sm.oid
+
+        @self.appbuilder.sm.oid.require_login
+        def handle_login():
+            user = sm.auth_user_oid(oidc.user_getfield('email'))
+
+            if user is None:
+                info = oidc.user_getinfo(
+                    ['sub', 'given_name', 'family_name', 'email']
+                )
+
+                # Superset requires `first_name` not to be NULL, so we use the
+                # `given_name` rather than `nickname`, which has higher
+                # probability for being NULL
+                user = sm.add_user(
+                    username=info.get('sub'),
+                    first_name=info.get('given_name'),
+                    last_name=info.get('family_name'),
+                    email=info.get('email'),
+                    role=sm.find_role(sm.auth_user_registration_role)
+                )
+
+            login_user(user, remember=False)
+            return redirect(self.appbuilder.get_url_for_index)
+
+        return handle_login()
+
+    @expose('/logout/', methods=['GET', 'POST'])
+    def logout(self):
+
+        oidc = self.appbuilder.sm.oid
+        id_token = oidc.get_access_token()
+
+        oidc.logout()
+        super(SupersetAuthOIDCView, self).logout()
+        redirect_url = request.url_root.strip(
+            '/') + self.appbuilder.get_url_for_login
+
+        issuer_uri = oidc.client_secrets.get('issuer')
+        logout_uri = f'{issuer_uri}/logout?id_token_hint={id_token}&redirect_uri='
+
+        if 'OIDC_LOGOUT_URI' in self.appbuilder.app.config:
+            logout_uri = self.appbuilder.app.config['OIDC_LOGOUT_URI']
+
+        return redirect(logout_uri + quote(redirect_url))
