82a492c5b8a367b76683f072fc8d0157503fca2b
Justin Carver <carver@justcarver.com>
2021-03-24 12:09:14 -0400

    Updated to support Airflow 2 packages

--- fab_oidc/security.py
+++ fab_oidc/security.py
@@ -3,11 +3,11 @@ from flask_appbuilder.security.sqla.manager import SecurityManager
 from flask_oidc import OpenIDConnect
 from .views import AuthOIDCView
 from logging import getLogger
+
 log = getLogger(__name__)
 
 
 class OIDCSecurityManagerMixin:
-
     def __init__(self, appbuilder):
         super().__init__(appbuilder)
         if self.auth_type == AUTH_OID:
@@ -20,19 +20,34 @@ class OIDCSecurityManager(OIDCSecurityManagerMixin, SecurityManager):
 
 
 try:
-    from airflow.www_rbac.security import AirflowSecurityManager
+    from airflow.www.security import AirflowSecurityManager
 
-    class AirflowOIDCSecurityManager(OIDCSecurityManagerMixin,
-                                     AirflowSecurityManager):
+    class AirflowOIDCSecurityManager(OIDCSecurityManagerMixin, AirflowSecurityManager):
         pass
+
+
 except ImportError:
-    log.debug('Airflow not installed')
+    log.debug("Airflow 2 not installed")
+
+    try:
+        from airflow.www_rbac.security import AirflowSecurityManager
+
+        class AirflowOIDCSecurityManager(
+            OIDCSecurityManagerMixin, AirflowSecurityManager
+        ):
+            pass
+
+    except ImportError:
+        log.debug("Airflow 1 not installed")
 
 try:
     from superset.security import SupersetSecurityManager
 
-    class SupersetOIDCSecurityManager(OIDCSecurityManagerMixin,
-                                      SupersetSecurityManager):
+    class SupersetOIDCSecurityManager(
+        OIDCSecurityManagerMixin, SupersetSecurityManager
+    ):
         pass
+
+
 except ImportError:
-    log.debug('Superset not installed')
+    log.debug("Superset not installed")
